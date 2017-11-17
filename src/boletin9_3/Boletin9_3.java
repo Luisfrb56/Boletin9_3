8c1d2c3805de9ebd9de55c5ba9bb85c100e39b10
adm <adm@LAPTOP-EQL1FQIM>
2017-11-17 11:31:54 +0100

    Boletin9_3

---
+++ src/boletin9_3/Boletin9_3.java
@@ -0,0 +1,13 @@
+
+package boletin9_3;
+
+
+public class Boletin9_3 {
+
+    
+    public static void main(String[] args) {
+      Metodos3 obj=new Metodos3();
+      obj.calcularArea();
+    }
+    
+}
---
+++ src/boletin9_3/Metodos3.java
@@ -0,0 +1,23 @@
+
+package boletin9_3;
+
+import java.util.Scanner;
+
+
+public class Metodos3 {
+    public void calcularArea(){
+    int base,altura,Area;
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Dame la base: ");
+    base=sc.nextInt();
+        System.out.println("Dame la altura: ");
+    altura=sc.nextInt();
+    if(base>=0 && altura>=0){
+        Area=base*altura;
+        System.out.println("El area del rectangulo es: "+Area);
+    }else{
+        System.out.println("Error:Numero añadido negativo");
+    }
+    
+}
+}
